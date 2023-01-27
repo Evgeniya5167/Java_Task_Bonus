5a4a0329f2dd3c5afba9f5ed6edde92a1e138454
Евгения <jane5167@mail.ru>
2023-01-27 20:00:39 +0300

    added .gitignore

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        int a = 118; // начальная сумма на счету
-        int b = 1678; // сумма пополнения
+        int balance = 120; // начальная сумма на счету
+        int deposit = 1670; // сумма пополнения
 
-        if (b > 1000) {
-            a = a + b + b / 100;
-            System.out.println("На счету " + a + " рублей, " + "добавлено " + b / 100 + " бонусов.");
+        if (deposit > 1000) {
+            balance = balance + deposit + deposit / 100;
+            System.out.println("На счету " + balance + " рублей, " + "добавлено " + deposit / 100 + " бонусов.");
         } else {
-            a = a + b;
-            System.out.println("На счету " + a + " рублей.");
+            balance = balance + deposit;
+            System.out.println("На счету " + balance + " рублей.");
         }
     }
 }
